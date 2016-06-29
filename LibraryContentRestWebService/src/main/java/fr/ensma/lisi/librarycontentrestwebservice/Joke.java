f630109afeb99e7c8dbb77eea012e0aa8ec77972
ib <ib@WINIB-MIQAEMB3J.local>
2016-06-29 15:00:58 +0200

    LibraryContentRestWebService 29 6 2016

---
+++ LibraryContentRestWebService/src/main/java/fr/ensma/lisi/librarycontentrestwebservice/Joke.java
@@ -0,0 +1,17 @@
+package fr.ensma.lisi.librarycontentrestwebservice;
+
+import lombok.Data;
+
+@Data
+public class Joke {
+
+	private int id;
+	private String joke;
+	private String [] categories; 
+	
+	private static final String [] Mes_Categorie = {"explicit", "nerdic"};
+
+	public static String[] getMesCategorie() {
+		return Mes_Categorie;
+	}
+}
---
+++ LibraryContentRestWebService/src/main/java/fr/ensma/lisi/librarycontentrestwebservice/JokeResponse.java
@@ -0,0 +1,13 @@
+package fr.ensma.lisi.librarycontentrestwebservice;
+
+import lombok.Data;
+
+@Data
+public class JokeResponse {
+	
+	private String type;
+	private Joke[] value;
+	
+	
+
+}
